7eafd47c9b5cfbfe111e2d84c10b56cd69862579
Sugeerth Murugesan <sugeerth@gmail.com>
2016-04-29 16:31:22 -0700

    added comments files

---
+++ BrainViewerDataPaths.py
@@ -0,0 +1,33 @@
+"""
+matrix_filename is a format where you will have an adjacency matrix in csv format 
+with the headers indicating the brain regions of interest
+
+centre_filename are coordinate positions of the brain regions for the spheres in the 
+anatomical layout 
+
+centres_abbreviation is the abbreviation of the brain regions in the header 
+
+template_filename provides with the outer brain atlas of the regions  
+
+parcelation_filename provides the parcellation of the brain regions 
+
+e.g. path
+matrix_filename = '/PATH_TO_YOUR_DATA/27nodeMatrix.csv'
+"""
+matrix_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/JesseDataset/27nodeMatrix.csv'
+centre_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/JesseDataset/27nodeCentres.csv'
+centres_abbreviation = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/JesseDataset/regions_199.txt'
+template_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/JesseDataset/ch2better.nii.gz'
+parcelation_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/JesseDataset/allROIs.nii.gz'
+
+#set the flag data around here for convenience, if you want to switch on or off 
+# the other windows
+GraphWindowShowFlag = True
+MainWindowShowFlag = True
+ElectrodeWindowShowFlag = False
+CorrelationTableShowFlag = True
+
+
+
+
+
---
+++ RunProjectMain.py
@@ -0,0 +1,7 @@
+import os,sys
+"""
+If you made it this far congrats!!
+CHANGE the paths below to visit and your directory
+"""
+os.environ["PYTHONPATH"] = "/YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis"
+os.system("/YOUR_PATH_TO_THE_VISIT_INSTALLATION_DIRECTORY/visit2.10.0/src/bin/visit -cli -uifile /YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis/BrainViewer.py")
