9355e0a19804f4c8939671ccf0247aee3130c05c
Sugeerth Murugesan <sugeerth@gmail.com>
2016-05-16 22:57:46 -0700

    new changes

--- brain-vis/BrainViewer.py
+++ brain-vis/BrainViewer.py
@@ -11,7 +11,7 @@ from PySide import QtCore, QtGui , QtUiTools
 
 import community as cm
 try:
-    # ... reading NIfTI 
+    # ... reading NIfT
     import nibabel as nib
     import numpy as np
     # import h5py
