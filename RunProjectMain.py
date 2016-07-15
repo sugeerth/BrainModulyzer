7dcea3c180b72a453d134e9bf562d1481b206ea7
Sugeerth Murugesan <sugeerth@gmail.com>
2016-07-14 21:36:29 -0700

    Changes to installation file

--- RunProjectMain.py
+++ RunProjectMain.py
@@ -4,4 +4,4 @@ If you made it this far congrats!!
 CHANGE the paths below to visit and your directory
 """
 os.environ["PYTHONPATH"] = "/YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis"
-os.system("/YOUR_PATH_TO_THE_VISIT_INSTALLATION_DIRECTORY/visit2.10.0/src/bin/visit -cli -uifile /YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis/BrainViewer.py")
+os.system("python /YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis/BrainViewer.py")
