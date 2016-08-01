79fec601be3678d39ab3b6d026108febd1a93735
Sugeerth Murugesan <sugeerth@gmail.com>
2016-08-01 15:15:13 -0700

    Update RunProjectMain.py

--- RunProjectMain.py
+++ RunProjectMain.py
@@ -3,5 +3,4 @@ import os,sys
 If you made it this far congrats!!
 CHANGE the paths below to visit and your directory
 """
-os.environ["PYTHONPATH"] = "/YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis"
 os.system("python /YOUR_PATH_TO_THE_INSTALLATION_DIRECTORY/brain-vis/BrainViewer.py")
