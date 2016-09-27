0e21b46339a520081e102f1d13e8b3310f9a4b6e
Sugeerth Murugesan <sugeerth@gmail.com>
2016-09-27 00:54:27 -0700

    License About button

---
+++ src/brain-vis/General_Interface/messageBox.py
@@ -0,0 +1,4 @@
+MESSAGE = "<p>Message boxes have a caption, a text, and up to three " \
+        "buttons, each with standard or custom texts.</p>" \
+        "<p>Click a button to close the message box. Pressing the Esc " \
+        "button will activate the detected escape button (if any).</p>"
