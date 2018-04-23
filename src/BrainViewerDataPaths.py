4516f67a9333460c57697575a51f280fddd03dbc
Sugeerth Murugesan <sugeerth@gmail.com>
2018-04-23 15:14:45 -0700

    Update BrainViewerDataPaths.py

--- src/BrainViewerDataPaths.py
+++ src/BrainViewerDataPaths.py
@@ -13,9 +13,9 @@ DataSetValue = 'Dataset1' # Change this to Dataset2 for other results
 CURR =  path.abspath(path.join(__file__ ,"../..")) # going one directory up 
 CURR = os.path.join(CURR, "SampleData/"+str(DataSetValue)+"/")
 
-matrix_filename = str(os.path.join(CURR,'27NodeMatrix.csv'))
-parcelation_filename = str(os.path.join(CURR, "allROIs.nii.gz"))
-template_filename = str(os.path.join(CURR, "ch2better.nii.gz"))
+matrix_filename = str(os.path.join(CURR,'27NodeMatrix.csv')) # "fc_mat_mean_hc_64.csv" for Dataset2
+parcelation_filename = str(os.path.join(CURR, "allROIs.nii.gz")) # "MNI152_T1_2mm_brain.nii.gz" for Dataset2
+template_filename = str(os.path.join(CURR, "ch2better.nii.gz")) # "all_parcels.nii.gz" for Dataset2, it maynot work first time, but you run the tool agin it will
 
 #set the flag data around here for convenience, if you want to switch on or off 
 # the other windows whenever you want
